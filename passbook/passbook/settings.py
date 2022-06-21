5e1a7544d47a307e12250b6fa5d62fd4aa6b2ebb
Yash0510 <yash.mishra@sparkeighteen.com>
2022-06-21 11:48:29 +0530

    added PageNumber Pagination in settings

--- passbook/passbook/settings.py
+++ passbook/passbook/settings.py
@@ -60,6 +60,11 @@ CORS_ORIGIN_WHITELIST = (
 
 ROOT_URLCONF = 'passbook.urls'
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 1
+}
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
