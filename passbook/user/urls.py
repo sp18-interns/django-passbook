085388e6053c526c3d9e3eb0cea03bbd62e63cff
parag-intern <parag.gunjal@sparkeighteen.com>
2022-06-22 22:34:42 +0530

    Added request and response with class based views

--- passbook/user/urls.py
+++ passbook/user/urls.py
@@ -1,6 +1,14 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
 from . import views
 
+
+#router = routers.DefaultRouter()
+#router.register('users', views.UserViewSet, basename='users')
+
 urlpatterns = [
-    path('', views.post_list, name='signup'),
+    #path('', include(router.urls)),
+    path('user', views.user_list),
+    path('user/<int:pk>', views.user_detail)
+
 ]
--- passbook/user/views.py
+++ passbook/user/views.py
@@ -1,4 +1,65 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import UserSerializer
+from .models import User
+
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import User
+from .serializers import UserSerializer
+
 
 # Create your views here.
 
+# class UserViewSet(viewsets.ModelViewSet):
+#     """
+#     Api endpoints that allows users to be viewed or edited.
+#     """
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
+#     permission_classes = [permissions.IsAuthenticated]
+
+@api_view(['GET', 'POST'])
+def user_list(request, format=None):
+    """
+    List all code snippets, or create a new user.
+    """
+    if request.method == 'GET':
+        snippets = User.objects.all()
+        serializer = UserSerializer(snippets, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def user_detail(request, pk, format=None):
+    """
+     Retrieve, update or delete a code
+    """
+    try:
+        user = User.objects.get(pk - pk)
+    except User.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
