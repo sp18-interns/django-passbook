0ed1bf4a6f7420a5cd5f3afda623866674a1b968
parag-intern <parag.gunjal@sparkeighteen.com>
2022-06-28 08:08:15 +0530

    Implementing auth

---
+++ passbook/user/auth.py
@@ -0,0 +1,11 @@
+# import jwt
+# from django.conf import settings
+#
+# def generate_access_token(user):
+#
+#     access_token_payload = {
+#
+#     }
+#     access_token = jwt.encode(access_token_payload,
+#                               settings.SECRET_KEY, algorithm='HS256')
+#     return access_token
